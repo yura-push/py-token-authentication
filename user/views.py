b0a9e45c01320d36e96b6468116c98c23459c626
Yura Push <youdeimos1@gmail.com>
2025-02-11 13:15:24 +0200

    instatiate CreateUserView

--- user/views.py
+++ user/views.py
@@ -1,1 +1,7 @@
-# write your code here
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
