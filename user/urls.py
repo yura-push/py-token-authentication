5f7d9745b8a4e8ace72ded39b24bedf39ea99c38
Yura Push <youdeimos1@gmail.com>
2025-02-11 13:58:27 +0200

    add url pattern for me endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import (
+    CreateUserView,
+    LoginUserView,
+    ManageUserView
+)
 
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create")
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="token"),
+    path("me/", ManageUserView.as_view(), name="manage")
 ]
 
 app_name = "user"
