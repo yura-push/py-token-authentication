b39733304b94862b2645c3167e14f5304234177b
Yura Push <youdeimos1@gmail.com>
2025-02-11 13:16:05 +0200

    add path for register endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,10 @@
-# write your code here
+from django.urls import path
+
+from user.views import CreateUserView
+
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create")
+]
+
+app_name = "user"
